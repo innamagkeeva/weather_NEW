26b50347f91ff199ca5c4e40bc71c277cdb756e7
innamagkeeva <inna.m79@mail.ru>
2024-11-10 03:49:01 +0300

    добавила функцию checkInput но она не работает

--- js/main.js
+++ js/main.js
@@ -2,10 +2,24 @@ import UI from './ui.js'
 // console.log(UI)
 
 // вешаю обработчика события на форму, и при нажатии на поиск или enter вызывается функция getCityName
-
 UI.FORM.addEventListener('submit', getCityName)
 UI.FOOTER_BUTTON.addEventListener('click', addCityName)
+
+//вешаю обработчика события на инпут , кот вызовет функ проверки инпута
+UI.FORM_INPUT.addEventListener('input', checkInput)
 console.log(UI.FORM)
+
+//
+function checkInput() {
+  const inputValue = UI.FORM_INPUT.value.trim() //переменная кот присвоили значение инпута
+  if (inputValue) {
+    //если в инпуте текст есть..........
+    UI.FORM_BUTTON.removeAttribute('disabled') // то атрибут, отключающий кнопку убирается и кнопка работает
+  } else {
+    //иначе (т е если текста нет)
+    UI.FORM_BUTTON.setAttribute('disabled', '') //то атрибут вставляется и отключает кнопку отправки
+  }
+}
 
 // Функция, которая которая берет название города , введенного в input. подставляет его в url адрес и отправляет на сервер. и получает ответ с данными погоды.
 function getCityName(e) {
@@ -41,51 +55,51 @@ function getCityName(e) {
         UI.PART_IMG.alt = 'картинка солнца'
       }
       console.log(data)
-
+      // вешаю обработчик соб на нижнее поле . при клике на сердеечко вызывается ф addLike
       UI.FOOTER.addEventListener('click', addLike)
     })
   console.log(cityName)
 }
 
 function addLike() {
-  UI.SVG_LIKE.classList.add('like')
+  UI.SVG_LIKE.classList.add('like') //добавляется класс лайк, в css которого команда окрасить сердечко в красный
 }
 
-function addCityName() {
+function addCityName(cityName) {
   //функция кот создает новый эл.в который добавляется кнопка с названием города и кнопка для удаления элемента. и этот эл добавляется в список.
   const newLi = document.createElement('li') //создаю новый элемент
   newLi.className = 'list_li' //присваиваю ему класс
 
-  newLi.append(createButton())
-  newLi.append(createButtonText()) //для всех новых ли
-  newLi.append(createButtonDelete()) //так же для всех новых ли
+  // cityName.push(normalizeNewText)
+  newLi.append(createButton()) // создаю кнопку, которая принимает значение - назв города.
+  newLi.append(createButtonDelete()) // для всех новых ли
   UI.LIST.append(newLi) //в список добавляю этот нов. элемент
+
+  // saveCityToLocalStorage(cityName)
 
   clearInput()
 }
 
+// function saveCityToLocalStorage(cityName) {
+//   localStorage.setItem(JSON.stringify(cityName))
+// }
+
 function clearInput() {
   UI.FORM_INPUT.value = ''
 }
+
 //функция создает кнопку в кот будет попадать назв города, которое мы искали. те ввели в поиск.
 function createButton() {
   const newButton = document.createElement('button') //создается новая кнопка
   newButton.className = 'list__button-city' //новой кнопке присв класс
+
+  const newText = UI.FORM_INPUT.value.trim()
+  const normalizeNewText = newText.charAt(0).toUpperCase() + newText.slice(1)
+  newButton.textContent = normalizeNewText
+
   console.log(newButton)
 
   return newButton
-}
-
-function createButtonText() {
-  const newButtonText = document.createElement('p')
-  newButtonText.className = 'list__button-text'
-  const newText = UI.FORM_INPUT.value.trim()
-  const normalizeNewText = newText.charAt(0).toUpperCase() + newText.slice(1) // ВОПРОС: в консоле выводит с пробелами и с мал буквы, а на сайте как надо
-
-  newButtonText.textContent = normalizeNewText
-  console.log(newButtonText)
-
-  return newButtonText // возвращается кнопка вместе с текстом-назв города из инпута.
 }
 
 function createButtonDelete() {
