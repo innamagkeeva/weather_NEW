61fd55cc387174d310ebe830c6a6a1c1e38d840c
innamagkeeva <inna.m79@mail.ru>
2024-11-23 02:51:35 +0300

    запрос работет

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 import UI from './ui.js'
-import { apiKey } from './const.js'
+import { apiKey, serverUrl } from './const.js'
 
 let favoriteCities = [] // создали переменную, в которую поместили пустой массив для добавления городов
 
@@ -13,7 +13,6 @@ function getCityName(e) {
   e.preventDefault() // отмена перезагрузки страницы
 
   const cityName = UI.FORM_INPUT.value.trim() //конст, кот присваивается значение инпута без пробелов
-  console.log('город', cityName)
   getCityWeather(cityName) //вызов функ кот отправляет запрос для получения данных с сервера
 
   clearInput() //инпут очищается
@@ -27,9 +26,7 @@ function addCityName() {
   //функция кот создает новый эл.в который добавляется кнопка с названием города и кнопка для удаления элемента. и этот эл добавляется в список.
 
   const cityName = UI.CITY_NAME.textContent.trim()
-  console.log('Добавление города:', cityName)
   if (!cityName || favoriteCities.includes(cityName)) {
-    console.log('Город пуст или уже есть в избранных. Не добавляем.')
     return // Не добавляем пустые названия или дубли
   }
 
@@ -38,7 +35,6 @@ function addCityName() {
   addCity(cityName)
 
   saveToLocalStorage() //вызываю функция,кот сохраняет данные в localStorage
-  console.log('Избранные города:', favoriteCities)
 
   console.log(favoriteCities)
 }
@@ -49,7 +45,6 @@ function saveToLocalStorage() {
 
 //загружает города из localStorage
 function loadCitiesFromLocalStorage() {
-  console.log('Загрузка городов из локального хранилища')
   const storedCities = JSON.parse(localStorage.getItem('favoriteCities')) || [] // переменная, кот присваиваем элемент из localStorage или пустой массив
   storedCities.forEach((city) => {
     addCity(city) // и для каждого города создается новый li
@@ -60,20 +55,16 @@ function addCity(cityName) {
   favoriteCities.push(cityName) // беру из localStorage название города и добавляю его в массив
   const newLi = document.createElement('li') // создаю новый элемент li
   newLi.className = 'list_li' // присваиваю ему класс
-  newLi.append(createButton(cityName)) // создаю кнопку для всех нов ли с названием города
-
   const newButton = createButton(cityName) //создаем перем в кот помещаем данные о новой кнопки
-  newButton.addEventListener('click', getCityWeather) // веш слуш соб на нов кнопку.При клике на нее отравляется запрос на сервер для получения данных о погоде данного города
+  newLi.append(newButton) // создаю кнопку для всех нов ли с названием города
+
+  newButton.addEventListener('click', () => getCityWeather(cityName)) // веш слуш соб на нов кнопку.При клике на нее отравляется запрос на сервер для получения данных о погоде данного города
 
   newLi.append(createButtonDelete()) // создаю кнопку удаления для всех новых ли
   UI.LIST.append(newLi) // добавляем элемент в список
 }
-console.log(1111111, UI.LIST)
 
 function getCityWeather(cityName) {
-  console.log('Запрос данных о погоде для города:', cityName)
-  const serverUrl = 'https://api.openweathermap.org/data/2.5/weather'
-
   const url = `${serverUrl}?q=${cityName}&appid=${apiKey}`
 
   if (!cityName) {
@@ -119,8 +110,6 @@ function createButton(cityName) {
   const newButton = document.createElement('button') //создается новая кнопка
   newButton.className = 'list__button-city' //новой кнопке присв класс
   newButton.textContent = cityName // текст контент у кнопки
-
-  console.log('это новая кнопка', newButton)
 
   return newButton
 }
